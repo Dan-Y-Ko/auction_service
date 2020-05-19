5368cd7eb51caf937d924b27a3839bcb441d43f4
Dan <danko56666@gmail.com>
2020-05-19 02:20:46 -0400

    add validation to prevent bidding on own auction or bid where user is already highest bidder

--- auction-service/src/handlers/placeBid.js
+++ auction-service/src/handlers/placeBid.js
@@ -12,13 +12,26 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function placeBid(event, context) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
+  const { email } = event.requestContext.authorizer;
 
   const auction = await getAuctionById(id);
 
+  // bid identity validation
+  if (email === auction.seller) {
+    throw new createError.Forbidden("You cannot bid on your own auctions!");
+  }
+
+  // avoid double bidding
+  if (email === auction.highestBid.bidder) {
+    throw new createError.Forbidden("You are already the highest bidder");
+  }
+
+  // auction status validation
   if (auction.status === "CLOSED") {
     throw new createError.Forbidden("You cannot bid on closed auctions!");
   }
 
+  // bid amount validation
   if (amount <= auction.highestBid.amount) {
     throw new createError.Forbidden(
       `Your bid must be higher than ${auction.highestBid.amount}`
@@ -28,9 +41,11 @@ async function placeBid(event, context) {
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
-    UpdateExpression: "set highestBid.amount = :amount",
+    UpdateExpression:
+      "set highestBid.amount = :amount, highestBid.bidder = :bidder",
     ExpressionAttributeValues: {
       ":amount": amount,
+      ":bidder": email,
     },
     ReturnValues: "ALL_NEW",
   };
