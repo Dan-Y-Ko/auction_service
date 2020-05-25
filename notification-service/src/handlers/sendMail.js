f7f66f1cba4119d55b5d129f200bbbdb6aabf5c4
Dan <danko56666@gmail.com>
2020-05-25 18:34:06 -0400

    implement test send mail handler function

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,6 +1,31 @@
+import AWS from "aws-sdk";
+
+const ses = new AWS.SES({ region: "us-east-1" });
+
 async function sendMail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: "danko56666@gmail.com",
+    Destination: {
+      ToAddresses: ["danko56666@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "Hello",
+        },
+      },
+      Subject: {
+        Data: "Test",
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
